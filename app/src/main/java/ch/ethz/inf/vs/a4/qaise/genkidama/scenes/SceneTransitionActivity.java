0addf43fe6233cadb326d25e853a7f3e1c3f959d
Cinderella25 <cinderella25.cindy25@gmail.com>
2017-11-17 16:58:50 +0100

    17.11.17 mit SceneTransitionActivity

---
+++ app/src/main/java/ch/ethz/inf/vs/a4/qaise/genkidama/scenes/SceneTransitionActivity.java
@@ -0,0 +1,15 @@
+package ch.ethz.inf.vs.a4.qaise.genkidama.scenes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ch.ethz.inf.vs.a4.qaise.genkidama.R;
+
+public class SceneTransitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scene_transition);
+
+    }
+}
